4a7f5ca6852e843ab4b5ffd8dac3f8962a472c41
Administrator <Administrator@DESKTOP-HONP501>
2024-03-17 20:56:37 +0900

    3.17_할수있는데까지만

--- board/src/main/java/com/example/demo/board/mapper/BoardMapper.java
+++ board/src/main/java/com/example/demo/board/mapper/BoardMapper.java
@@ -8,23 +8,20 @@ public interface BoardMapper {
 	//1일차 과제
 	
 	List<BoardVO> getBoardList(BoardVO bvo); //어떻게 넘길지 모르니 sql문 먼저 선행.
-	//앞의 BoardVO 는 resultType 이고 뒤의 BoardVO는 파라미터인데 이건 생략 가능...
+	//앞의 BoardVO 는 resultType 이고 뒤의 BoardVO는 파라미터인데 이건 생략 가능...	
 	
-	//검색 (제목, 내용, 작성자 검색 동적쿼리로 구현)
-	
-	
-	//페이징
-	
-	
+	//검색 (제목, 내용, 작성자 검색 동적쿼리로 구현) , 페이징??
+		
 	//단건조회
 	BoardVO getBoardInfo(int boardNo);
 	
 	//등록
-	int insertBoard(BoardVO boardNo);
+	int insertBoard(BoardVO bvo);
 	
 	//삭제
-	
+	int deleteBoard(int boardNO);
 	
 	//수정 (단건선행?)
+	int updateBoard(BoardVO bvo);
 
 }
--- board/src/test/java/com/example/demo/board/BoardMapperTest.java
+++ board/src/test/java/com/example/demo/board/BoardMapperTest.java
@@ -12,20 +12,39 @@ public class BoardMapperTest {
 	
 	@Autowired BoardMapper mapper;
 	
+	//게시글 검색, 페이징
 	//@Test
-	public void 등록() {
+	public void boardList() {
+
+	}
+
+	//단건조회
+	//@Test
+	public void boardInfo() {
 		
 	}
 	
+	//등록
 	//@Test
-	public void 수정() {
+	public void 등insertBoard() {
+		
+	}
+	
+	//삭제
+	//@Test
+	public void deleteBoard() {
+		int boardNo = 2;
+		int result = mapper.deleteBoard(boardNo);
+		System.out.println("삭제 : "  + result);
+		
+	}
+	
+	//수정
+	//@Test
+	public void updateBoard() {
 			
 	}
 
-	//@Test
-	public void 삭제() {
-		
-	}
 	
 		
 
